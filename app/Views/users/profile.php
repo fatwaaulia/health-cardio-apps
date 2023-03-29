f09c10ccbdf2cd9fb90d5d16369ba13b0fbf8ee3
Fatwa Aulia <fatwaaulia.fy@gmail.com>
2023-03-29 15:31:47 +0700

    dselect internal

--- app/Views/users/profile.php
+++ app/Views/users/profile.php
@@ -366,3 +366,11 @@ function changePassconf() {
     }
 }
 </script>
+
+<script src="<?= base_url().'/assets/js/dselect.js' ?>"></script>    
+<script>
+dselect(document.querySelector('#jenis_kelamin'));
+dselect(document.querySelector('#riwayat_diabetes'));
+dselect(document.querySelector('#riwayat_alkohol'));
+dselect(document.querySelector('#riwayat_merokok'));
+</script>
