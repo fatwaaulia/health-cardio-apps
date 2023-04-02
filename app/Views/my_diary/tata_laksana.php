a19bca9c6820e85521ec10e28819dfb7e2757ca2
Fatwa Aulia <fatwaaulia.fy@gmail.com>
2023-04-02 09:22:57 +0700

    border-radius

--- app/Views/my_diary/tata_laksana.php
+++ app/Views/my_diary/tata_laksana.php
@@ -6,7 +6,7 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-lg-12">
+            <div class="col-lg-4">
                 <div class="card p-3">
                     <a href="<?= str_replace('_','-',$name).'/edit/'.model('M_Env')->encode($data['id']) ?>">
                         <?php
@@ -20,6 +20,16 @@
                         ?>
                         <button class="btn <?= $btn_risiko ?>"><?= $data['risiko'] ?></button>
                     </a>
+                    <button class="btn mt-3" style="background-color:#ffc3ae">
+                        <div class="row">
+                            <div class="col-2">
+                                <img src="<?= base_url('assets/img/heart.png') ?>" class="wh-50">
+                            </div>
+                            <div class="col-10 my-auto">
+                                <label class="fw-600 float-start">Edukasi Penyakit</label>
+                            </div>
+                        </div>
+                    </button>
                 </div>
             </div>
         </div>
